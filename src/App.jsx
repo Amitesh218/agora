ad784fb96c96d9247dc5a035966cfc06f363436c
Amitesh218 <amiteshrawal1@gmail.com>
2025-03-02 19:39:58 +0530

    changed to react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className="relative  min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
+  )
+}
+export default App
