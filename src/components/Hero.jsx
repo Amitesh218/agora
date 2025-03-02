038e2217063b254e6e1b5862892de26d2fa6acee
Amitesh218 <amiteshrawal1@gmail.com>
2025-03-02 20:15:26 +0530

    updated hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Hero = () => {
     return(
-        <div>Hero</div>
+        <div className="relative h-dvh">
+            <div id="video-frame" classname="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+                <div>
+                    <div classname>
+                        
+                    </div>
+                </div>
+            </div>        
+        </div>
+
     )
 }
 
